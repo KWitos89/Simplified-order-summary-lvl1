5447b48a7d0d48319ded5626b6b1ab5eb41db397
Krzysztof Witos <kwitos89@gmail.com>
2023-03-13 17:46:22 +0100

    js css circle btn

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 const btn = document.querySelector(".card__btn-priamry");
 
 const loading = (e) => {
-    const circle = document.createElement('span')
-    circle.classList.add('active');
+    // const circle = document.createElement('span')
+    // circle.classList.add('active');
 
-
-    if (!btn.classList.contains('charging--btn')) {
-        e.target.appendChild(circle)
-        btn.classList.add('charging--btn')
-    } else if (circle.classList.contains('active')) {
-        btn.classList.remove('charging--btn')
-        circle.classList.remove('active')
-    }
+btn.classList.toggle('btn--circle')
+    // if (!btn.classList.contains('charging--btn')) {
+    //     e.target.appendChild(circle)
+    //     btn.classList.add('charging--btn')
+    // } else if (circle.classList.contains('active')) {
+    //     btn.classList.remove('charging--btn')
+    //     circle.classList.remove('active')
+    // }
 
 };
 btn.addEventListener('click', loading);
