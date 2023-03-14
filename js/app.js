3d5f9b5d0251f379e9a6869c9128efedbfc4b12f
Krzysztof Witos <kwitos89@gmail.com>
2023-03-14 21:09:03 +0100

    added js function and finished styling the card

--- js/app.js
+++ js/app.js
@@ -1,17 +1,24 @@
 const btn = document.querySelector(".card__btn-priamry");
+const box = document.querySelector(".box--effect");
+const textbtn = document.querySelector(".btn--text");
+const checkIcon = document.querySelector(".check");
 
-const loading = (e) => {
-    // const circle = document.createElement('span')
-    // circle.classList.add('active');
+const loading = () => {
 
-btn.classList.toggle('btn--circle')
-    // if (!btn.classList.contains('charging--btn')) {
-    //     e.target.appendChild(circle)
-    //     btn.classList.add('charging--btn')
-    // } else if (circle.classList.contains('active')) {
-    //     btn.classList.remove('charging--btn')
-    //     circle.classList.remove('active')
-    // }
+    textbtn.textContent = "";
+    btn.style.cursor = "wait";
+    box.classList.add("loanding");
+    btn.style.backgroundColor = "rgba(73, 2, 69, 1)";
+    btn.style.boxShadow = "10px 15px 25px -10px rgba(73, 2, 69, 1), -10px 15px 25px -10px rgba(73, 2, 69, .8)";
+
+    setTimeout(() => {
+        box.classList.remove("loanding");
+        btn.classList.add("done");
+        checkIcon.classList.add("check--active");
+        btn.style.pointerEvents = "none";
+
+    }, 4000);
 
 };
+
 btn.addEventListener('click', loading);
